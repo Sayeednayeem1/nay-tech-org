eda9e6529a22f96327811e91ca986f67d7dbc147
Abu Sayeed <sayeed@pop-os.localdomain>
2023-03-01 19:21:31 +0600

    added particle animation as background

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 
 import { RouterProvider } from 'react-router-dom';
+import Particle from './Pages/Particles/Particle';
 import { router } from './router/router';
 
 function App() {
   return (
-    <div className='bg-gradient-to-tr from-[#CEB9DB] to-[#E2D7EA] bg-cover'>
-      <RouterProvider router={router}></RouterProvider>
+    <div className='bg-gradient-to-tr from-[#CEB9DB] to-[#E2D7EA] bg-cover' style={{ height: '80vh' }}>
+      <Particle></Particle>
+      <div>
+        <RouterProvider router={router}></RouterProvider>
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
-// import { RouterProvider } from 'react-router-dom';
-// import ParticlesBg from 'particles-bg';
-// import './App.css';
-// import { router } from './router/router';
-
-// function App() {
-//   return (
-//     <div >
-//       <ParticlesBg type="color" bg={true} />
-//       <RouterProvider router={router}></RouterProvider>
-//     </div>
-//   );
-// }
-// // todo className='bg-gradient-to-tr from-[#CEB9DB] to-[#E2D7EA] bg-cover'
-
-// export default App;
